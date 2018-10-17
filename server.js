eb89c54e7e2e685e0ee551ae1b0d2fd381a65011
Eduardo da Silva <eduardo.2599@hotmail.com>
2018-10-17 17:59:50 -0300

    Instalado o modulo requireDir e aplicado o mesmo para dar required

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const requireDir = require('require-dir');
 //iniciando o app
 const app = express();
 
 //iniciando o DB
 
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
-require('./src/models/Products')
+requireDir('./src/models')
 
 //primera rota
 app.get('/', (req, res) => {
