bb00a959e3333bf851d4e90a4e27026abdbd3113
Eduardo da Silva <eduardo.2599@hotmail.com>
2018-10-17 17:57:41 -0300

    Criado o model de Produto e importado o mesmo no servidor.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 //iniciando o DB
 
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
+require('./src/models/Products')
 
 //primera rota
 app.get('/', (req, res) => {
